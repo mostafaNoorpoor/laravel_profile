393702d665c9e364ff5cf489d39d2d0d6d28d11d
mostafaNurpur <44763967+mostafaNurpur@users.noreply.github.com>
2020-01-02 10:03:27 +0330

    Update User.php

--- profile/app/User.php
+++ profile/app/User.php
@@ -2,14 +2,16 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
+
 
 class User extends Authenticatable
 {
-    use Notifiable;
-    
+    use Notifiable , HasRoles ;
+
     protected $table = "users" ;
 
     /**
@@ -28,5 +30,14 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
     ];
 }

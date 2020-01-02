1b2392035c4d435d9ab94bfb543aa80c3d7f4541
mostafaNurpur <44763967+mostafaNurpur@users.noreply.github.com>
2020-01-02 10:02:53 +0330

    Update MessageController.php

--- profile/app/Http/Controllers/MessageController.php
+++ profile/app/Http/Controllers/MessageController.php
@@ -1,14 +1,75 @@
 <?php
-
 namespace App\Http\Controllers;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
 use App\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class MessageController extends Controller
 {
+    public function setRoleAndPermission(Request $request){
+        $user = User::create([
+        	'name' => ''.$request->name.'', 
+        	'email' => ''.$request->email.'',
+        	'password' => ''.Hash::make($request->password).''
+        ]);
+  
+        $role = Role::create(['name' => $request->role]);
+   
+        $permissions = Permission::pluck('id','id')->all();
+  
+        $role->syncPermissions($permissions);
+   
+        $user->assignRole([$role->id]);
+        return $user ;
+    }
+    public function userRoles($id){
+        $usersElequent = new User ;
+        $roles = Role::all();
+        $Permission = Permission::all();
+        $users = $usersElequent::with('roles')->where('id',$id)->get();
+        // $nonmembers = $users->reject(function ($user, $key) {
+        //     return $user->hasRole('member');
+        // });
+        return response()->json(['roles'=>$roles, 'Permission' => $Permission ,  'users' => $users], 200);
+    }
+    public function getDataByName(){
+        // send name like admin and it return who are admins
+        $usersElequent = new User ;
+
+        return $usersElequent::whereHas("roles", function($q){ $q->where("name", "Admin"); })->get();
+    }
+    public function checkAdminOrUser($userId){
+        $userModel = new User ;
+        $roles = Role::all();
+        $userData = $userModel::with('roles')->where('id' , $userId)->get();
+        if (count($userData[0]['roles']) > 0){
+            return response()->json([ "data" => $userData[0]['roles'][0]['name'] , "success"=> 1  ], 200);
+        } else {
+            return response()->json([ "data" => "user" , "success"=> 2  ], 404);
+        }
+    }
+    public function checkIfAdmin(){
+        $user = new User ;
+        $whichuser = $user::find(1);
+        $roles = $whichuser->getRoleNames();
+        return $roles[0] ;
+    }
+    public function checkAdmin(Request $request){
+        $user = new User ;
+        if ($user::where('id', $request->id)->exists()) {
+            $userRole = $user::find(1);
+            $roles = $userRole->getRoleNames();
+            return response()->json([ "roles" => $roles  , "success"=> 1  ], 200);    
+        } else {
+            return response()->json([ "message" => "user not found" , "success"=> 2  ], 404);
+
+        }
+    }
     public function getUserData($id){
         $user = new User ;
         if($user::where('id', $id)->exists()){

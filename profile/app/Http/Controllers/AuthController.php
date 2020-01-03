9e30ada6429e1c206557aca2d9f5becdb783a36f
mostafaNurpur <44763967+mostafaNurpur@users.noreply.github.com>
2020-01-03 14:52:37 +0330

    Update AuthController.php

--- profile/app/Http/Controllers/AuthController.php
+++ profile/app/Http/Controllers/AuthController.php
@@ -3,66 +3,55 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
 use App\User;
+use Illuminate\Support\Facades\Hash;
+use App\Repository\generateToken\Passport;
+use App\Repository\User\register as UserRegister;
 
 class AuthController extends Controller
 {
     public function signup(Request $request)
     {
-        // $request->validate([
-        //     'name' => 'required|string',
-        //     'email' => 'required|string|email|unique:users',
-        //     'password' => 'required|string|confirmed'
-        // ]);
-        $user = new User([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => bcrypt($request->password)
+        $request->validate([
+            'name' => 'required|string',
+            'family_name' => 'required|string',
+            'phone_number' => 'required|string',
+            'email' => 'required|string',
         ]);
-        $user->save();
+        $register = new UserRegister;
         return response()->json([
             'message' => 'Successfully created user!'
         ], 201);
     }
-
-    public function login(Request $request)
+    public function logIn(Request $request)
     {
-        // $request->validate([
-        //     'email' => 'required|string|email',
-        //     'password' => 'required|string',
-        //     'remember_me' => 'boolean'
-        // ]);
-        $credentials = request(['email', 'password']);
-        if(!Auth::attempt($credentials))
-            return response()->json([
-                'message' => 'Unauthorized'
-            ], 401);
-        $user = $request->user();
-        $tokenResult = $user->createToken('Personal Access Token');
-        $token = $tokenResult->token;
-        if ($request->remember_me)
-            $token->expires_at = Carbon::now()->addWeeks(1);
-        $token->save();
-        return response()->json([
-            'access_token' => $tokenResult->accessToken,
-            'token_type' => 'Bearer',
-            'expires_at' => Carbon::parse(
-                $tokenResult->token->expires_at
-            )->toDateTimeString()
+        $request->validate([
+            'phoneNumber' => 'required|string',
+            'OTP' => 'required|string',
         ]);
+        $user = new User();
+        $logIn = $user::where('phone_number', $request->phoneNumber)->first();
+        if (Hash::check($request->OTP, $logIn->OTP)) {
+            $generateGrantToken = new Passport;
+            $tokens = $generateGrantToken::createTokenPassport($request->phoneNumber, $request->OTP);
+            return $tokens;
+        } else {
+            return response()->json([
+                'message' => 'not match'
+            ], 401);
+        }
     }
-  
+
     public function logout(Request $request)
     {
-         $request->user()->token()->revoke();
+        $request->user()->token()->revoke();
         return response()->json([
             'message' => 'Successfully logged out'
         ]);
     }
 
-    public function user(Request $request)
+    public function userData(Request $request)
     {
         return response()->json($request->user());
     }

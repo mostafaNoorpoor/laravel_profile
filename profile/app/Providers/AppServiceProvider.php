b74332428908442306fa971e9c398db51a91956d
mostafaNurpur <44763967+mostafaNurpur@users.noreply.github.com>
2020-01-03 14:52:50 +0330

    Update AppServiceProvider.php

--- profile/app/Providers/AppServiceProvider.php
+++ profile/app/Providers/AppServiceProvider.php
@@ -1,10 +1,10 @@
 <?php
 
 namespace App\Providers;
-use Illuminate\Support\Facades\Schema;
-
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
 
 class AppServiceProvider extends ServiceProvider
 {

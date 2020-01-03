65705616ecfe522dcef13788c63c071b983e7230
mostafaNurpur <44763967+mostafaNurpur@users.noreply.github.com>
2020-01-03 14:55:27 +0330

    Update api.php

--- profile/routes/api.php
+++ profile/routes/api.php
@@ -2,51 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::get('/user/{id}', 'MessageController@getUserData');
-
-Route::post('/registerNewUser', 'MessageController@registerNewUser');
-
-Route::post('/updateProfile', 'MessageController@updateUser');
-
-//Route::post('/logIn', 'MessageController@logIn');
-
-Route::post('/checkAdmin', 'MessageController@checkAdmin');
-
-Route::get('/userRoles/{id}', 'MessageController@userRoles');
-
-Route::get('/checkAdminOrUser/{id}', 'MessageController@checkAdminOrUser');
-
-Route::post('/setRoleAndPermission', 'MessageController@setRoleAndPermission');
-
-// Route::post('/login', 'AuthController@login');
-
-// Route::post('/signup', 'AuthController@signup');
-
-// Route::get('/logout', 'AuthController@logout');
-
-// Route::get('/user', 'AuthController@user');
+Route::post('/setOTP', 'UserController@setOTP');
 
 Route::group([
     'prefix' => 'auth'
 ], function () {
     Route::post('login', 'AuthController@login');
     Route::post('signup', 'AuthController@signup');
-  
+
     Route::group([
-      'middleware' => 'auth:api'
-    ], function() {
+        'middleware' => 'auth:api'
+    ], function () {
         Route::get('logout', 'AuthController@logout');
-        Route::get('user', 'AuthController@user');
+        Route::get('user', 'AuthController@userData');
     });
 });

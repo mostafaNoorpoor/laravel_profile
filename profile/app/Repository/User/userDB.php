d766985408dbe1c3d7c485cf42f0414cb6bc372a
mostafaNurpur <44763967+mostafaNurpur@users.noreply.github.com>
2020-01-03 15:24:42 +0330

    Update userDB.php

--- profile/app/Repository/User/userDB.php
+++ profile/app/Repository/User/userDB.php
@@ -20,6 +20,18 @@ class userDB implements userInterFace
         return $toUpdateOTP->save();
     }
 
+    public static function ifUserExistByPhoneNumber($phoneNumber)
+    {
+        $user = new User();
+        return $user::where('phone_number', $phoneNumber)->exists();
+    }
+
+    public static function getUserData($phoneNumber)
+    {
+        $user = new User();
+        return $user::where('phone_number', $phoneNumber)->first();
+    }
+
     public function updateData()
     {
     }
